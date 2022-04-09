516eecaefbcbd9542042419bd13d164b54326820
Jorian <jorian@mail.ru>
2022-04-09 17:43:06 +0300

    authors list, one article and less corrections

--- habr/blueprints/posts.py
+++ habr/blueprints/posts.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
 
 from habr.models.post import Post
+from habr.models.user import User
 
 themes_dic = {
     'design': 'Дизайн',
@@ -20,13 +21,33 @@ posts = Blueprint(
 @posts.route('/')
 def post_list():
     postlist = Post.query.order_by(Post.created_at.desc()).all()
-    return render_template('index.html', menu=None, title="Главная страница",
+    return render_template('index.html', menu='main', title="Главная страница",
                            postlist=postlist)
 
 
 @posts.route("/theme/<theme_name>/")
 def theme_filter(theme_name: str):
+    if theme_name not in themes_dic.keys():
+        raise NotFound('Нет такой категории!')
     postlist = Post.query.filter(Post.category == theme_name.upper()).order_by(
-        Post.created_at.desc())
+        Post.created_at.desc()).all()
+    print(postlist)
     return render_template('index.html', menu=theme_name,
                            title=themes_dic[theme_name], postlist=postlist)
+
+
+@posts.route("/author/<int:pk>/")
+def author_filter(pk: int):
+    author = User.query.filter_by(id=pk).first_or_404()
+    postlist = Post.query.filter_by(user_id=pk).order_by(
+        Post.created_at.desc()).all()
+    title = f'Все статьи автора {author.username}'
+    print(postlist)
+    return render_template('index.html', title=title, postlist=postlist)
+
+
+@posts.route('/post/<int:pk>')
+def concrete_post(pk: int):
+    selected_post = Post.query.filter_by(id=pk).first_or_404()
+    title = selected_post.users.username + ' «' + selected_post.header + '»'
+    return render_template('article.html', post=selected_post, title=title)
